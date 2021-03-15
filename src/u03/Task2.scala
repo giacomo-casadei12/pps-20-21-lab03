6e721e731d60efa47a4f4ac50f2e6897e37e5cb2
Giacomo Casadei <giacomo.casadei12@studio.unibo.it>
2021-03-15 12:52:05 +0100

    Task2 done

--- src/u03/Task2.scala
+++ src/u03/Task2.scala
@@ -1,5 +1,17 @@
 package u03
 
+import u03.Lists.List._
+import Lists._
+import u02.Optionals.Option
+
 object Task2 {
+
+  def max(l: List[Int]): Option[Int] = {
+    def _max(l:List[Int], n:Option[Int]): Option[Int] = l match {
+      case Cons(h,_) => _max(filter(l)(_>h),Option.Some(h))
+      case _ => n
+    }
+    _max(l,Option.None())
+  }
 
 }
--- test/u03/Task2Test.scala
+++ test/u03/Task2Test.scala
@@ -1,5 +1,18 @@
 package u03
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import u03.Lists.List
+import u03.Lists.List._
+import Task2._
+import u02.Optionals.Option
 
 object Task2Test {
+
+  val l = Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
+
+  @Test def testMax() = {
+    assertEquals(Option.Some(30),max(l))
+    assertEquals(Option.None(),max(Nil()))
+  }
 
 }
